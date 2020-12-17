20da72825b0e74ad43ade1e5e2f4a152066c0291
Doug Ransom <doug.ransom@gmail.com>
2020-12-17 12:40:27 -0800

    directories/files moved for flit conventions

---
+++ build_package.ps1
@@ -0,0 +1,5 @@
+#run this in powershell
+#build an sdist (source distribution) package for vocola
+#no wheel is built, one less thing to worry about
+#use the no-setup option 
+flit build --format sdist  --no-setup-py
